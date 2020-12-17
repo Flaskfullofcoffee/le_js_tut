884a122f7e56e0891ca10a2acaae5f8c0a912070
Flaskfullofcoffee <zdobbs3@gmail.com>
2020-12-17 13:01:31 -0700

    moved main.js into scripts folder

--- main.js
+++
@@ -1,3 +0,0 @@
-let Phrase = require(`@flaskfullofcoffee/palindrome`);
-
-alert(new Phrase(`Madam, I'm Adam.`).isPalindrome());
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+let Phrase = require(`@flaskfullofcoffee/palindrome`);
+
+// alert(new Phrase(`Madam, I'm Adam.`).isPalindrome());
+let string = prompt("Please enter a string for palindrome testing:");
